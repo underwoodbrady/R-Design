cdecb48bd53cacc8068bcfab1469c54e20efbe6a
underwood.brady <underwood.brady@gmail.com>
2024-10-20 19:55:48 -0700

    footer mostly done for now

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,30 +1,56 @@
 <script lang="ts">
     
-    import Up from 'virtual:icons/material-symbols/keyboard-arrow-up-rounded';
     import Instagram from 'virtual:icons/mdi/instagram';
     import Facebook from 'virtual:icons/mdi/facebook';
     import Twitter from 'virtual:icons/mdi/twitter';
+    import Email from 'virtual:icons/mdi/email-outline';
 
 </script>
 
-<footer class="w-full flex flex-col items-center pt-12 pb-5 px-6 bg-primary-12 crimson-text">
-    <div class="flex flex-col items-center space-y-1 text-mauve-3">
-        <Up/>
-        <p class=" font-semibold">Back To Top</p>
+<footer class="w-full  px-xl pt-3xl pb-lg bg-primary-12 flex-flex-col">
+    <div class="flex justify-between">
+        <div>
+            <img src="/LogoSVG(dark).svg" alt="Logo Dark" class="w-32 mb-lg"/>
+            <p class="text-primary-3 max-w-md">Seasonal staging and design team bringing the magic of the holidays to your home or office, located In Seattle, WA</p>
+        </div>
+        <div class="flex space-x-3xl">
+            <div>
+                <h3 class="text-primary-5 text-xl marcellus mb-md">Navigation</h3>
+                <ul class="text-primary-1 flex flex-col space-y-sm">
+                    <li class="hover:text-primary-9 duration-100 transition-colors"><a href="/">
+                        Home
+                    </a></li>
+                    <li class="hover:text-primary-9 duration-100 transition-colors"><a href="/gallery">Gallery</a></li>
+                    <li class="hover:text-primary-9 duration-100 transition-colors"><a href="/services">Services</a></li>
+                    <li class="hover:text-primary-9 duration-100 transition-colors"><a href="/blog">Blog</a></li>
+                    <li class="hover:text-primary-9 duration-100 transition-colors"><a href="/">Contact</a></li>
+                </ul>
+            </div>
+            <!--  w-[205px] h-[41px] -->
+            <div>
+                <div class="mb-lg">
+                    <h3 class="text-primary-5 text-xl marcellus mb-md">Join Our Mailing List</h3>
+                    <div class="relative w-[205px] h-[41px] border border-primary-5 rounded-sm">
+                        <input name='email' placeholder="Enter Email" class=" pl-2xl absolute inset-0 rounded-sm outline-none bg-transparent text-primary-1"/>   
+                        <Email class="absolute left-3 top-[10px] text-primary-5"/>
+                    </div>
+                    
+                </div>
+                <div>
+                    <h3 class="text-primary-5 text-xl marcellus mb-md">Follow Us</h3>
+                    <div class="flex-center space-x-md text-primary-1 text-lg">
+                        <Instagram class="hover:text-primary-9 duration-100 transition-colors"/>
+                        <Facebook class="hover:text-primary-9 duration-100 transition-colors"/>
+                        <Twitter class="hover:text-primary-9 duration-100 transition-colors"/>
+                
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <img src="Logo2.png" alt="Logo" class="w-44 my-9"/>
-    <ul class="text-mauve-9 flex flex-col items-center space-y-4 font-semibold text-xl ">
-        <li>About</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Blog</li>
-        <li>Contact</li>
-    </ul>
-    <div class="flex items-center space-x-3 text-iris-11 text-lg my-12">
-        <Instagram/>
-        <Facebook/>
-        <Twitter/>
-
+    <div class="w-full h-[1px] bg-primary-9 mt-3xl mb-lg"/>
+    <div>
+        <p class="text-primary-9 text-sm">Copyright © 2024-2025, Spruce Seasonal Staging And Decor</p>
     </div>
-    <p class="text-mauve-3">Copyright ©2024</p>
+  
 </footer>
